73f7a7d0bb4e605c71084e674e2936e8c43d24ee
Khushi Garg <khushigarg.64901@gmail.com>
2023-09-24 19:30:55 +0530

    updated email veriyf

--- services/userAuthServices.js
+++ services/userAuthServices.js
@@ -240,8 +240,8 @@ class userAuthServices {
         ) {
             throw new ApiBadRequestError("OTP has Expired");
         }
-        if (checkUser.email_otp == OTP || true) {
-            // if (String(checkUser.email_otp) == String(OTP) ) {
+        // if (checkUser.email_otp == OTP || true) {
+        if (String(checkUser.email_otp) == String(OTP)) {
             checkUser.is_email_verified = true;
             await checkUser.save();
         }
